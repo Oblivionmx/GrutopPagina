2d152a2442a01c217f88d363f1734cbdab628112
maximiliano <maximiliano12joseph@gmail.com>
2024-10-13 17:46:28 -0500

    Header gaaa

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-console.log("Hola")
+document.querySelector('.menu-item').addEventListener('click', function() {
+    const dropdown = document.querySelector('.dropdown');
+    dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+});
+
+document.addEventListener('click', function(event) {
+    const menu = document.querySelector('.menu');
+    const isClickInside = menu.contains(event.target);
+
+    if (!isClickInside) {
+        document.querySelector('.dropdown').style.display = 'none';
+    }
+});
